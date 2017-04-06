31112673039c692d662dd07fdee19d3908a66a9e
Nils Schaetti <nils.schaetti@unine.ch>
2017-04-06 13:17:00 +0200

    docs

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(name='EchoTorch',
       author_email='nils.schaetti@unine.ch',
       license='GPLv3',
       packages=['echotorch'],
-      zip_safe=False
+      zip_safe=False,
+      dependency_links=[
+            'http://download.pytorch.org/whl/torch-0.1.10.post1-cp35-cp35m-macosx_10_6_x86_64.whl'
+      ]
       )
 
