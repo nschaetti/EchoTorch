7f236c02b0191677a73c171e58fd92de7d98b738
Nils Schaetti <nils.schaetti@unine.ch>
2017-04-06 12:49:20 +0200

    docs

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='EchoTorch',
       author='Nils Schaetti',
       author_email='nils.schaetti@unine.ch',
       license='GPLv3',
-      packages=['echotorch'],
+      packages=['dataset', 'nn', 'tools'],
       zip_safe=False,
       install_requires=[
           'torch',
