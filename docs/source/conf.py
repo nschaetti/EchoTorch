9abe235c618e529ba68ad09c37405591e22ae36d
Nils Schaetti <nils.schaetti@unine.ch>
2017-04-06 12:13:42 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,10 +16,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
+#import os
+#import sys
 #import sphinx_bootstrap_theme
-sys.path.insert(0, os.path.abspath('../../echotorch'))
+#sys.path.insert(0, os.path.abspath('../../echotorch'))
 
 
 # -- General configuration ------------------------------------------------
