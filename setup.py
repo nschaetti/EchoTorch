d17ecd8d8a203fab8c7756f697587d9aec2c282c
Nils Schaetti <nils.schaetti@unine.ch>
2017-04-06 13:20:32 +0200

    docs

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(name='EchoTorch',
       license='GPLv3',
       packages=['echotorch'],
       zip_safe=False,
+      install_requires=[
+            'torchvision'
+      ],
       dependency_links=[
             'http://download.pytorch.org/whl/torch-0.1.10.post1-cp35-cp35m-macosx_10_6_x86_64.whl'
       ]
