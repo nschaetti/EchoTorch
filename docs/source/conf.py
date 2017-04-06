91aa22836f5080a2686019bc2d65f3e9bf3402d8
Nils Schaetti <nils.schaetti@unine.ch>
2017-04-06 13:29:06 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-#import sphinx_bootstrap_theme
+import sphinx_bootstrap_theme
 sys.path.insert(0, os.path.abspath('../../echotorch'))
 
 
@@ -87,8 +87,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_bootstrap_theme'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
