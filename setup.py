a5ab253c0f47ac5a633bf250ffa69f376699b99d
Nils Schaetti <nils.schaetti@unine.ch>
2017-04-06 12:40:43 +0200

    docs

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='EchoTorch',
+      version='0.1',
+      description="A Python toolkit for Reservoir Computing.",
+      long_description="A Python toolkit for Reservoir Computing and Echo State Network experimentation based on pyTorch.",
+      author='Nils Schaetti',
+      author_email='nils.schaetti@unine.ch',
+      license='GPLv3',
+      packages=['echotorch'],
+      zip_safe=False,
+      install_requires=[
+          'torch',
+          'Sphinx',
+          'numpy',
+          'scipy'
+          ],
+      )
