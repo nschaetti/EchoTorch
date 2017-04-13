3b8859e7fdd22f8e63f3163e6162b46f9d515d8c
Nils Schaetti <nils.schaetti@unine.ch>
2017-04-13 14:00:15 +0200

    docs

--- test.py
+++ test.py
@@ -10,19 +10,16 @@ if __name__ == "__main__":
 
     reservoir_size = 100
     input_size = 2
+    output_size = 2
 
     x = Variable(torch.zeros(reservoir_size), requires_grad=False)
     u = Variable(torch.ones(input_size), requires_grad=True)
     win = Variable(torch.rand(reservoir_size, input_size), requires_grad=False)
     w = Variable(torch.rand(reservoir_size, reservoir_size), requires_grad=False)
-    winu = win.mv(u)
-    print("winu : ")
-    print(winu)
-    wx = w.mv(x)
-    print("wx : ")
-    print(wx)
-    x = F.tanh(winu + wx)
-    print(x)
-    print(x.creator)
+    x = F.tanh(win.mv(u) + w.mv(x))
+    ll = nn.Linear(reservoir_size, output_size)
+    p = ll(x)
+    print(p)
+    print(p.creator)
 
 # end if
