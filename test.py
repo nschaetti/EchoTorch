260eb36a29c66a13ec8ed6231a0e2f4709cd63e0
Nils Schaetti <nils.schaetti@unine.ch>
2017-04-13 15:41:06 +0200

    Basic Echo State Network

--- test.py
+++ test.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     # Variable
     u = Variable(torch.rand(10, 2))
 
+    initial_state = Variable(torch.rand(100), requires_grad=False)
+
     # ESN
     esn = echotorch.nn.Reservoir(2, 2, 100, bias=False)
-    esn(u)
+    esn(u, initial_state)
 
 # end if
