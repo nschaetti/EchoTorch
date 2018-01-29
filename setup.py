bd266179face1d74c64ccf406aa4f06c36bdeb18
Nils Schaetti <nils.schaetti@unine.ch>
2018-01-29 17:04:13 +0100

    find_packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='EchoTorch',
       version='0.1',
@@ -7,7 +7,7 @@ setup(name='EchoTorch',
       author='Nils Schaetti',
       author_email='nils.schaetti@unine.ch',
       license='GPLv3',
-      packages=['echotorch'],
+      packages=find_packages(),
       install_requires=[
           'torch',
           'numpy',
