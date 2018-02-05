a04b37caa197834c35dc3531958adff9fdf8897f
Nils Schaetti <nils.schaetti@unine.ch>
2018-02-05 17:05:38 +0100

    init

---
+++ echotorch/transforms/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+from .TextTransformer import TextTransformer
+from .WVTextTransformer import WVTextTransformer
+
+__all__ = [
+    'TextTransformer', 'WVTextTransformer'
+]
