617ba6163ec4dbc2004d80260a1b6c92a1b58aca
nschaetti <n.schaetti@gmail.com>
2018-02-07 20:53:11 +0100

    init

---
+++ echotorch/transforms/text/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+from .Character import Character
+from .FunctionWord import FunctionWord
+from .GloveVector import GloveVector
+from .PartOfSpeech import PartOfSpeech
+from .Tag import Tag
+from .Token import Token
+from .Transformer import Transformer
+
+__all__ = [
+    'Character', 'FunctionWord', 'Transformer', 'GloveVector', 'PartOfSpeech', 'Tag', 'Token'
+]
