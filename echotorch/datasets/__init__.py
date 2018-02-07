086008ec3a6b32e7ff650801b27c08468e5d77f9
nschaetti <n.schaetti@gmail.com>
2018-02-07 20:50:41 +0100

    ini

--- echotorch/datasets/__init__.py
+++ echotorch/datasets/__init__.py
@@ -6,7 +6,8 @@ from .MackeyGlassDataset import MackeyGlassDataset
 from .MemTestDataset import MemTestDataset
 from .NARMADataset import NARMADataset
 from .ReutersC50Dataset import ReutersC50Dataset
+from .SFGramDataset import SFGramDataset
 
 __all__ = [
-    'MackeyGlassDataset', 'MemTestDataset', 'NARMADataset', 'ReutersC50Dataset'
+    'MackeyGlassDataset', 'MemTestDataset', 'NARMADataset', 'ReutersC50Dataset', 'SFGramDataset'
 ]
