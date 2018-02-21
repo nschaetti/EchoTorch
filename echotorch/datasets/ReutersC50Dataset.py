efcb394c84b46170aa2085e7b4527c3db77d089c
nschaetti <n.schaetti@gmail.com>
2018-02-21 14:42:44 +0100

    Save transform

--- echotorch/datasets/ReutersC50Dataset.py
+++ echotorch/datasets/ReutersC50Dataset.py
@@ -11,6 +11,8 @@ import json
 import codecs
 from random import shuffle
 import math
+import pickle
+from datetime import datetime
 
 
 # Reuters C50 dataset
@@ -20,7 +22,8 @@ class ReutersC50Dataset(Dataset):
     """
 
     # Constructor
-    def __init__(self, root='./data', download=False, n_authors=50, dataset_size=100, authors=None, transform=None, train=True, k=10):
+    def __init__(self, root='./data', download=False, n_authors=50, dataset_size=100, dataset_start=0, authors=None,
+                 transform=None, train=True, k=10, retain_transform=False, load_transform=False):
         """
         Constructor
         :param root: Data root directory.
@@ -29,11 +32,13 @@ class ReutersC50Dataset(Dataset):
         :param dataset_size: How many samples from each author to load (1 to 100).
         :param authors: The list of authors name to load.
         :param transform: A TextTransformer object to apply.
+        :param retain_transform:
         """
         # Properties
         self.root = root
         self.n_authors = n_authors if authors is None else len(authors)
         self.dataset_size = dataset_size
+        self.dataset_start = dataset_start
         self.authors = authors
         self.transform = transform
         self.author2id = dict()
@@ -42,6 +47,13 @@ class ReutersC50Dataset(Dataset):
         self.train = train
         self.k = k
         self.fold = 0
+        self.retain_transform = retain_transform
+        self.load_transform = load_transform
+
+        # Check path
+        if self.dataset_size * self.n_authors < self.k:
+            self.k = self.dataset_size * self.n_authors
+        # end if
 
         # Create directory if needed
         if not os.path.exists(self.root):
@@ -87,6 +99,16 @@ class ReutersC50Dataset(Dataset):
         self._select_data()
     # end set_train
 
+    # Set start
+    def set_start(self, start):
+        """
+        Set start
+        :param start:
+        :return:
+        """
+        self.dataset_start = start
+    # end set_start
+
     #############################################
     # OVERRIDE
     #############################################
@@ -109,13 +131,32 @@ class ReutersC50Dataset(Dataset):
         """
         # Current file
         text_path, author_name = self.fold_texts[idx]
-
+        # print(text_path)
         # Read text
         text_content = codecs.open(text_path, 'r', encoding='utf-8').read()
 
         # Transform
         if self.transform is not None:
-            return self.transform(text_content), self.author2id[author_name]
+            # Load transform
+            transformed = None
+            if self.load_transform:
+                transformed = self._load_transform(text_path, type(self.transform).__name__)
+            # end if
+
+            # Transform if not found
+            if transformed is None:
+                transformed, transformed_size = self.transform(text_content)
+                to_be_saved = True
+            else:
+                to_be_saved = False
+            # end if
+
+            # Save transform
+            if to_be_saved and self.retain_transform:
+                self._save_transform(transformed, text_path, type(self.transform).__name__)
+            # end if
+
+            return transformed, self.author2id[author_name], self._create_labels(author_name, transformed_size)
         else:
             return text_content, self.author2id[author_name]
         # end if
@@ -124,6 +165,53 @@ class ReutersC50Dataset(Dataset):
     ##############################################
     # PRIVATE
     ##############################################
+
+    # Save transform
+    def _save_transform(self, transform, text_path, transform_name):
+        """
+        Save transform
+        :param text_path:
+        :param transform_name:
+        :return:
+        """
+        print(u"load")
+        return pickle.dump(transform, open(text_path + "." + transform_name, 'wb'))
+    # end _save_transform
+
+    # Load transform
+    def _load_transform(self, text_path, transform_name):
+        """
+        Load transform
+        :param text_path:
+        :return:
+        """
+        print(u"load")
+        try:
+            return pickle.load(open(text_path + "." + transform_name, 'rb'))
+        except IOError:
+            return None
+        # end try
+    # end if
+
+    # Create labels
+    def _create_labels(self, author_name, length):
+        """
+        Create labels
+        :param author_name:
+        :param length:
+        :return:
+        """
+        # Author id
+        author_id = self.author2id[author_name]
+
+        # Vector
+        tag_vector = torch.zeros(length, self.n_authors)
+
+        # Set
+        tag_vector[:, author_id] = 1.0
+
+        return tag_vector
+    # end _create_labels
 
     # Create the root directory
     def _create_root(self):
@@ -167,23 +255,32 @@ class ReutersC50Dataset(Dataset):
         # Author count
         author_count = 0
 
+        # Given authors
+        if self.authors is not None:
+            given_authors = list(self.authors)
+        else:
+            given_authors = None
+        # end if
+        self.authors = list()
+
         # For each authors
         for index, author_name in enumerate(authors_info.keys()):
             # If in the set
-            if author_count < self.n_authors and (self.authors is None or author_name in self.authors):
+            if author_count < self.n_authors and (given_authors is None or author_name in given_authors):
                 # New author
                 self.author2id[author_name] = index
                 self.id2author[index] = author_name
 
                 # Add each text
                 for text_index, text_name in enumerate(authors_info[author_name]):
-                    if text_index < self.dataset_size:
+                    if text_index >= self.dataset_start and text_index < self.dataset_start + self.dataset_size:
                         # Add
                         self.texts.append((os.path.join(self.root, text_name + ".txt"), author_name))
                     # end if
                 # end for
 
                 # Count
+                self.authors.append(author_name)
                 author_count += 1
             # end if
         # end for
