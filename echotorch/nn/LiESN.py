3a79bdedaa99a95ef7e6488682e0ead675521ee3
nschaetti <n.schaetti@gmail.com>
2018-02-21 14:44:41 +0100

    LiESN constructor parameters

--- echotorch/nn/LiESN.py
+++ echotorch/nn/LiESN.py
@@ -60,18 +60,17 @@ class LiESN(ESN):
         :param leaky_rate:
         :param train_leaky_rate:
         """
-        super(LiESN, self).__init__(input_dim, hidden_dim, output_dim, spectral_radius=0.9,
-                                    bias_scaling=0, input_scaling=1.0,
-                                    w=None, w_in=None, w_bias=None, sparsity=None, input_set=[1.0, -1.0],
-                                    w_sparsity=None,
-                                    nonlin_func=torch.tanh, learning_algo='inv', ridge_param=0.0, create_cell=False)
+        super(LiESN, self).__init__(input_dim, hidden_dim, output_dim, spectral_radius=spectral_radius,
+                                    bias_scaling=bias_scaling, input_scaling=input_scaling,
+                                    w=w, w_in=w_in, w_bias=w_bias, sparsity=sparsity, input_set=input_set,
+                                    w_sparsity=w_sparsity, nonlin_func=nonlin_func, learning_algo=learning_algo,
+                                    ridge_param=ridge_param, create_cell=False)
 
         # Recurrent layer
-        self.esn_cell = LiESNCell(leaky_rate, train_leaky_rate, input_dim, hidden_dim, spectral_radius=0.9,
-                                  bias_scaling=0, input_scaling=1.0,
-                                  w=None, w_in=None, w_bias=None, sparsity=None, input_set=[1.0, -1.0],
-                                  w_sparsity=None,
-                                  nonlin_func=torch.tanh)
+        self.esn_cell = LiESNCell(leaky_rate, train_leaky_rate, input_dim, hidden_dim, spectral_radius=spectral_radius,
+                                  bias_scaling=bias_scaling, input_scaling=input_scaling,
+                                  w=w, w_in=w_in, w_bias=w_bias, sparsity=sparsity, input_set=input_set,
+                                  w_sparsity=w_sparsity, nonlin_func=nonlin_func)
     # end __init__
 
     ###############################################
