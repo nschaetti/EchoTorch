08e3a9867612a47e465abaeab3523f1a6b907f57
nschaetti <n.schaetti@gmail.com>
2018-02-21 14:43:26 +0100

    Bias to linear output

--- echotorch/nn/ESN.py
+++ echotorch/nn/ESN.py
@@ -24,6 +24,7 @@ Created on 26 January 2018
 @author: Nils Schaetti
 """
 
+import torch.sparse
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
@@ -40,7 +41,7 @@ class ESN(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, spectral_radius=0.9, bias_scaling=0, input_scaling=1.0,
                  w=None, w_in=None, w_bias=None, w_fdb=None, sparsity=None, input_set=[1.0, -1.0], w_sparsity=None,
                  nonlin_func=torch.tanh, learning_algo='inv', ridge_param=0.0, create_cell=True,
-                 feedbacks=False):
+                 feedbacks=False, with_bias=True):
         """
         Constructor
         :param input_dim: Inputs dimension.
@@ -66,6 +67,7 @@ class ESN(nn.Module):
         self.learning_algo = learning_algo
         self.ridge_param = ridge_param
         self.feedbacks = feedbacks
+        self.with_bias = with_bias
 
         # Recurrent layer
         if create_cell:
@@ -77,9 +79,16 @@ class ESN(nn.Module):
         if learning_algo == 'grad':
             self.linear = nn.Linear(hidden_dim, output_dim, bias=True)
         else:
+            # Size
+            if self.with_bias:
+                self.x_size = hidden_dim + 1
+            else:
+                self.x_size = hidden_dim
+            # end if
+
             # Set it as buffer
-            self.register_buffer('xTx', Variable(torch.zeros(hidden_dim, hidden_dim), requires_grad=False))
-            self.register_buffer('xTy', Variable(torch.zeros(hidden_dim, output_dim), requires_grad=False))
+            self.register_buffer('xTx', Variable(torch.zeros(self.x_size, self.x_size), requires_grad=False))
+            self.register_buffer('xTy', Variable(torch.zeros(self.x_size, output_dim), requires_grad=False))
             self.register_buffer('w_out', Variable(torch.zeros(1, hidden_dim), requires_grad=False))
         # end if
     # end __init__
@@ -108,9 +117,60 @@ class ESN(nn.Module):
         return self.esn_cell.w
     # end w
 
+    # Input matrix
+    @property
+    def w_in(self):
+        """
+        Input matrix
+        :return:
+        """
+        return self.esn_cell.w_in
+    # end w_in
+
     ###############################################
     # PUBLIC
     ###############################################
+
+    # Reset learning
+    def reset(self):
+        """
+        Reset learning
+        :return:
+        """
+        if self.learning_algo == 'grad':
+            self.linear.reset_parameters()
+        else:
+            self.xTx.data = torch.zeros(self.x_size, self.x_size)
+            self.xTy.data = torch.zeros(self.x_size, self.output_dim)
+            self.w_out.data = torch.zeros(1, self.output_dim)
+        # end if
+
+        # Training mode again
+        self.train(True)
+    # end reset
+
+    # Output matrix
+    def get_w_out(self):
+        """
+        Output matrix
+        :return:
+        """
+        if self.learning_algo == 'grad':
+            return self.linear.weight
+        else:
+            return self.w_out
+        # end if
+    # end get_w_out
+
+    # Set W
+    def set_w(self, w):
+        """
+        Set W
+        :param w:
+        :return:
+        """
+        self.esn_cell.w = w
+    # end set_w
 
     # Forward
     def forward(self, u, y=None):
@@ -133,11 +193,16 @@ class ESN(nn.Module):
             hidden_states = self.esn_cell(u)
         # end if
 
+        # Add bias
+        if self.with_bias:
+            hidden_states = self._add_constant(hidden_states)
+        # end if
+
         # Learning algo
         if self.learning_algo != 'grad' and self.training:
             for b in range(batch_size):
                 self.xTx.data.add_(hidden_states[b].t().mm(hidden_states[b]).data)
-                self.xTy.data.add_(hidden_states[b].t().mm(y[b].unsqueeze(1)).data)
+                self.xTy.data.add_(hidden_states[b].t().mm(y[b]).data)
             # end for
             return hidden_states
         elif self.learning_algo != 'grad' and not self.training:
@@ -194,5 +259,16 @@ class ESN(nn.Module):
     ###############################################
     # PRIVATE
     ###############################################
+
+    # Add constant
+    def _add_constant(self, x):
+        """
+        Add constant
+        :param x:
+        :return:
+        """
+        bias = Variable(torch.ones((x.size()[0], x.size()[1], 1)), requires_grad=False)
+        return torch.cat((bias, x), dim=2)
+    # end _add_constant
 
 # end ESNCell
