c72f2ffad57ba0d4e8b3416e6023491e9eeaf5f8
nschaetti <n.schaetti@gmail.com>
2018-04-07 15:39:00 +0200

    nn init

--- echotorch/nn/__init__.py
+++ echotorch/nn/__init__.py
@@ -6,7 +6,11 @@ from .ESNCell import ESNCell
 from .ESN import ESN
 from .LiESNCell import LiESNCell
 from .LiESN import LiESN
+from .ICACell import ICACell
+from .PCACell import PCACell
+from .RRCell import RRCell
+from .SFACell import SFACell
 
 __all__ = [
-    'ESNCell', 'ESN', 'LiESNCell', 'LiESN'
+    'ESNCell', 'ESN', 'LiESNCell', 'LiESN', 'ICACell', 'PCACell', 'RRCell', 'SFACell'
 ]
