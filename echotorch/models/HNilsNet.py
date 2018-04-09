0dfc2249a3d2cd33a5f611db64779c9b75e317cb
Nils Schaetti <n.schaetti@gmail.com>
2018-04-09 17:47:21 +0200

    Hierarchical NilsNet Canvas

---
+++ echotorch/models/HNilsNet.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# File : echotorch/models/NilsNet.py
+# Description : A Hierarchical NilsNet module.
+# Date : 09th of April, 2018
+#
+# This file is part of EchoTorch.  EchoTorch is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Nils Schaetti, University of Neuchâtel <nils.schaetti@unine.ch>
+
+# Imports
+import torchvision
+import torch.nn as nn
+
+
+# A Hierarchical NilsNet
+class HNilsNet(nn.Module):
+    """
+    A Hierarchical NilsNet
+    """
+
+    # Constructor
+    def __init__(self):
+        """
+        Constructor
+        """
+        pass
+    # end __init__
+
+    # Forward
+    def forward(self):
+        """
+        Forward
+        :return:
+        """
+        pass
+    # end forward
+
+# end HNilsNet
