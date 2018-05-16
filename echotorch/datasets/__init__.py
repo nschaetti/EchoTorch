b63fa1ddd91c3ce51db480f599fe446bde77dad5
nschaetti <n.schaetti@gmail.com>
2018-05-16 14:41:37 +0200

    Removed Reuters C50 and SFGram

--- echotorch/datasets/__init__.py
+++ echotorch/datasets/__init__.py
@@ -6,9 +6,7 @@ from .LogisticMapDataset import LogisticMapDataset
 from .MackeyGlassDataset import MackeyGlassDataset
 from .MemTestDataset import MemTestDataset
 from .NARMADataset import NARMADataset
-from .ReutersC50Dataset import ReutersC50Dataset
-from .SFGramDataset import SFGramDataset
 
 __all__ = [
-    'LogisticMapDataset', 'MackeyGlassDataset', 'MemTestDataset', 'NARMADataset', 'ReutersC50Dataset', 'SFGramDataset'
+    'LogisticMapDataset', 'MackeyGlassDataset', 'MemTestDataset', 'NARMADataset'
 ]
