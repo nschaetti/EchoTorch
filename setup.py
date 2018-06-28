ef960b3c06bab8b386729ad4ea040e61659d3bd9
nschaetti <n.schaetti@gmail.com>
2018-06-28 11:21:39 +0200

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='EchoTorch',
-      version='0.1.1',
+      version='0.1.2',
       description="A Python toolkit for Reservoir Computing.",
       long_description="A Python toolkit for Reservoir Computing and Echo State Network experimentation based on pyTorch.",
       author='Nils Schaetti',
