78ea7705c53ce397932e8fbc0bb93a78c18513c4
nschaetti <n.schaetti@gmail.com>
2019-01-25 16:00:44 +0100

    Dataset to compose from other datasets

---
+++ echotorch/datasets/DatasetComposer.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+import torch
+from torch.utils.data.dataset import Dataset
+import numpy as np
+
+
+# Dataset Composer
+class DatasetComposer(Dataset):
+    """
+    The Rössler attractor is the attractor for the Rössler system, a system of three non-linear ordinary differential
+    equations originally studied by Otto Rössler. These differential equations define a continuous-time dynamical
+    system that exhibits chaotic dynamics associated with the fractal properties of the attractor.
+    """
+
+    # Constructor
+    def __init__(self, datasets, *args, **kwargs):
+        """
+        Constructor
+        :param datasets:
+        """
+        # Super
+        super(DatasetComposer, self).__init__(*args, **kwargs)
+
+        # Properties
+        self.datasets = datasets
+        self.n_datasets = len(datasets)
+
+        # Map item to datasets items
+        self.map_items = {}
+        self.n_samples = 0
+        index = 0
+        for i, d in enumerate(datasets):
+            for j in range(len(d)):
+                self.map_items[index] = (i, j)
+                index += 1
+                self.n_samples += 1
+            # end for
+        # end for
+    # end __init__
+
+    #############################################
+    # OVERRIDE
+    #############################################
+
+    # Length
+    def __len__(self):
+        """
+        Length
+        :return:
+        """
+        return self.n_samples
+    # end __len__
+
+    # Get item
+    def __getitem__(self, idx):
+        """
+        Get item
+        :param idx:
+        :return:
+        """
+        (d, e) = self.map_items[idx]
+        sample = self.datasets[d][e]
+        # print(d, e)
+        outputs = self._create_outputs(d, sample.shape[0])
+        # print(outputs[:10])
+        return self.datasets[d][e], outputs, torch.LongTensor([d])
+    # end __getitem__
+
+    #############################################
+    # PRIVATE
+    #############################################
+
+    # Create outputs
+    def _create_outputs(self, i, time_length):
+        """
+        Create outputs
+        :param i:
+        :param time_length:
+        :return:
+        """
+        # print("create {}".format(i))
+        # Create tensor
+        outputs = torch.zeros(time_length, self.n_datasets)
+
+        # Put to one
+        outputs[:, i] = 1.0
+
+        return outputs
+    # end _create_outputs
+
+# end DatasetComposer
