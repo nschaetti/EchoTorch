406146d8c48f7847c19f0691f2215034b37b8eb3
nschaetti <n.schaetti@gmail.com>
2019-01-25 16:04:59 +0100

    Datasets

--- echotorch/datasets/__init__.py
+++ echotorch/datasets/__init__.py
@@ -2,11 +2,17 @@
 #
 
 # Imports
+from .DatasetComposer import DatasetComposer
+from .HenonAttractor import HenonAttractor
 from .LogisticMapDataset import LogisticMapDataset
+from .LorenzAttractor import LorenzAttractor
 from .MackeyGlassDataset import MackeyGlassDataset
 from .MemTestDataset import MemTestDataset
 from .NARMADataset import NARMADataset
+from .RosslerAttractor import RosslerAttractor
+from .SinusoidalTimeseries import SinusoidalTimeseries
 
 __all__ = [
-   'LogisticMapDataset', 'MackeyGlassDataset', 'MemTestDataset', 'NARMADataset'
+   'DatasetComposer', 'HenonAttractor', 'LogisticMapDataset', 'LorenzAttractor', 'MackeyGlassDataset',
+   'MemTestDataset', 'NARMADataset', 'RosslerAttractor', 'SinusoidalTimeseries'
 ]
