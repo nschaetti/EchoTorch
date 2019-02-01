bfb921e50332d5be0a4ba89e91d4d1f8f8ab2775
nschaetti <n.schaetti@gmail.com>
2019-02-01 22:12:09 +0100

    dtype, distribs

--- echotorch/nn/LiESN.py
+++ echotorch/nn/LiESN.py
@@ -40,7 +40,9 @@ class LiESN(ESN):
                  bias_scaling=0, input_scaling=1.0, w=None, w_in=None, w_bias=None, sparsity=None,
                  input_set=[1.0, -1.0], w_sparsity=None, nonlin_func=torch.tanh, learning_algo='inv', ridge_param=0.0,
                  leaky_rate=1.0, train_leaky_rate=False, feedbacks=False, wfdb_sparsity=None,
-                 normalize_feedbacks=False, softmax_output=False, seed=None, washout=0):
+                 normalize_feedbacks=False, softmax_output=False, seed=None, washout=0, w_distrib='uniform',
+                 win_distrib='uniform', wbias_distrib='uniform', win_normal=(0.0, 1.0), w_normal=(0.0, 1.0),
+                 wbias_normal=(0.0, 1.0), dtype=torch.float32):
         """
         Constructor
         :param input_dim:
@@ -68,7 +70,9 @@ class LiESN(ESN):
                                     w_sparsity=w_sparsity, nonlin_func=nonlin_func, learning_algo=learning_algo,
                                     ridge_param=ridge_param, create_cell=False, feedbacks=feedbacks,
                                     wfdb_sparsity=wfdb_sparsity, normalize_feedbacks=normalize_feedbacks,
-                                    softmax_output=softmax_output, seed=seed, washout=washout)
+                                    softmax_output=softmax_output, seed=seed, washout=washout, w_distrib=w_distrib,
+                                    win_distrib=win_distrib, wbias_distrib=wbias_distrib, win_normal=win_normal,
+                                    w_normal=w_normal, wbias_normal=wbias_normal, dtype=torch.float32)
 
         # Recurrent layer
         self.esn_cell = LiESNCell(leaky_rate, train_leaky_rate, input_dim, hidden_dim, spectral_radius=spectral_radius,
@@ -76,7 +80,9 @@ class LiESN(ESN):
                                   w=w, w_in=w_in, w_bias=w_bias, sparsity=sparsity, input_set=input_set,
                                   w_sparsity=w_sparsity, nonlin_func=nonlin_func, feedbacks=feedbacks,
                                   feedbacks_dim=output_dim, wfdb_sparsity=wfdb_sparsity,
-                                  normalize_feedbacks=normalize_feedbacks, seed=seed)
+                                  normalize_feedbacks=normalize_feedbacks, seed=seed, w_distrib=w_distrib,
+                                  win_distrib=win_distrib, wbias_distrib=wbias_distrib, win_normal=win_normal,
+                                  w_normal=w_normal, wbias_normal=wbias_normal, dtype=torch.float32)
     # end __init__
 
     ###############################################
