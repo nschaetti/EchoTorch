71e567b3ef1f4f7e8ead17ee2ae348ae1e537b44
nschaetti <n.schaetti@gmail.com>
2019-02-06 07:01:20 +0100

    Set esn, get A, and A's SV

--- echotorch/nn/ConceptorPool.py
+++ echotorch/nn/ConceptorPool.py
@@ -41,7 +41,7 @@ class ConceptorPool(object):
     """
 
     # Contructor
-    def __init__(self, conceptor_dim, conceptors=list(), dtype=torch.float32):
+    def __init__(self, conceptor_dim, conceptors=list(), esn=None, dtype=torch.float32):
         """
         Constructor
         :param conceptors:
@@ -50,12 +50,29 @@ class ConceptorPool(object):
         self.conceptor_dim = conceptor_dim
         self.conceptors = conceptors
         self.name_to_conceptor = {}
+        self.esn = esn
         self.dtype = dtype
     # end __init__
 
     ###############################################
     # PROPERTIES
     ###############################################
+
+    # Singular values of A
+    @property
+    def A_SV(self):
+        """
+        Singular values of A
+        :return:
+        """
+        return ConceptorPool.compute_A_SV(self.conceptors)
+    # end A_SV
+
+    # A (OR of all conceptors)
+    @property
+    def A(self):
+        return ConceptorPool.compute_A(self.conceptors)
+    # end A
 
     # Quota
     @property
@@ -64,24 +81,39 @@ class ConceptorPool(object):
         Quota
         :return:
         """
-        # OR for all conceptor
-        for i, c in self.conceptors:
-            if i == 0:
-                M = c
-            else:
-                M = c.logical_or(M)
-            # nd if
-        # end for
-
-        # Quota is the sum of SV
-        return float(
-            torch.sum(M.mm(torch.eye(self.conceptor_dim, dtype=self.dtype))) / self.conceptor_dim
-        )
+        return ConceptorPool.compute_quota(self.conceptors)
     # end quota
 
     ###############################################
     # PUBLIC
     ###############################################
+
+    # Get similarity matrix
+    def similarity_matrix(self):
+        """
+        Get similarity matrix
+        :return:
+        """
+        # Similarity matrix
+        sim_matrix = torch.zeros(len(self.conceptors), len(self.conceptors))
+        for i, ca in enumerate(self.conceptors):
+            for j, cb in enumerate(self.conceptors):
+                sim_matrix[i, j] = ca.sim(cb)
+            # end for
+        # end for
+        return sim_matrix
+    # end similarity_matrix
+
+    # Finalize conceptor
+    def finalize_conceptor(self, i):
+        """
+        Finalize conceptor
+        :param i:
+        :return:
+        """
+        # Finalize
+        self.conceptors[i].finalize()
+    # end finalize_conceptor
 
     # Finalize all conceptors
     def finalize(self):
@@ -94,13 +126,119 @@ class ConceptorPool(object):
         # end for
     # end finalize
 
+    # Positive evidence
+    def E_plus(self, p):
+        """
+        Positive evidence
+        :param x: states (x)
+        :return:
+        """
+        # Sizes
+        batch_size = p.shape[0]
+        n_conceptors = len(self.conceptors)
+        time_length = p.shape[1]
+
+        # List of evidences
+        evidences = torch.zeros(batch_size, time_length, n_conceptors)
+
+        # Compute hidden states
+        x = self.esn(u=p, return_states=True)
+
+        # For each batch
+        for b in range(batch_size):
+            # Time
+            for t in range(time_length):
+                # For each conceptors
+                for i, c in enumerate(self.conceptors):
+                    evidences[b, t, i] = torch.mm(x[b, t].view(1, -1), c.get_C()).mm(x[b, t].view(-1, 1))
+                # end for
+            # end for
+        # end for
+        return torch.mean(evidences, dim=1)
+    # end E_plus
+
+    # Evidence for other
+    def E_other(self, p):
+        """
+        Evidence for other
+        :param p:
+        :return:
+        """
+        # Sizes
+        batch_size = p.shape[0]
+        time_length = p.shape[1]
+
+        # List of evidences
+        evidences = torch.zeros(batch_size, time_length)
+
+        # Compute hidden states
+        x = self.esn(u=p, return_states=True)
+
+        # All conceptors
+        A = self.A
+
+        # Not A
+        N = A.logical_not()
+
+        # For each batch
+        for b in range(batch_size):
+            # Time
+            for t in range(time_length):
+                evidences[b, t] = torch.mm(x[b, t].view(1, -1), N.get_C()).mm(x[b, t].view(-1, 1))
+            # end for
+        # end for
+        return torch.mean(evidences, dim=1)
+    # end E_other
+
+    # Negative evidence
+    def E_neg(self, p):
+        """
+        Negative evidence
+        :param p:
+        :return:
+        """
+        # Sizes
+        batch_size = p.shape[0]
+        n_conceptors = len(self.conceptors)
+        time_length = p.shape[1]
+
+        # List of evidences
+        evidences = torch.zeros(batch_size, time_length, n_conceptors)
+
+        # Compute hidden states
+        x = self.esn(u=p, return_states=True)
+
+        # For each batch
+        for b in range(batch_size):
+            # Time
+            for i, c in enumerate(self.conceptors):
+                # List of all conceptor without c
+                other_c = list(self.conceptors)
+                other_c.remove(c)
+
+                # Compute A
+                A = ConceptorPool.compute_A(other_c)
+
+                # Not A
+                N = A.logical_not()
+
+                # For each conceptors
+                for t in range(time_length):
+                    evidences[b, t, i] = torch.mm(x[b, t].view(1, -1), N.get_C()).mm(
+                        x[b, t].view(-1, 1))
+                # end for
+            # end for
+        # end for
+        return torch.mean(evidences, dim=1)
+    # end E_neg
+
     # Evidence for each conceptor
-    def E(self):
+    def E(self, p):
         """
         Evidence for each conceptor
         :return:
         """
-        pass
+        return self.E_plus(p) + self.E_neg(p)
     # end E
 
     # New conceptor
@@ -164,16 +302,6 @@ class ConceptorPool(object):
         # end if
     # end __getitem__
 
-    # Get attribute
-    def __getattr__(self, item):
-        """
-        Get attribute
-        :param item:
-        :return:
-        """
-        return self.name_to_conceptor[item]
-    # end __getattr__
-
     # Length
     def __len__(self):
         """
@@ -182,5 +310,62 @@ class ConceptorPool(object):
         """
         return len(self.conceptors)
     # end __len__
+
+    ###############################################
+    # STATIC
+    ###############################################
+
+    # Get singular values of A
+    @staticmethod
+    def compute_A_SV(conceptors):
+        """
+        Get singular values of A
+        :param conceptors:
+        :return:
+        """
+        # A (OR of all conceptors)
+        A = ConceptorPool.compute_A(conceptors)
+
+        # Compute SVD
+        _, S, _ = torch.svd(A.get_C())
+
+        return S
+    # end compute_A_SV
+
+    # Compute A (OR of all conceptors
+    @staticmethod
+    def compute_A(conceptors):
+        """
+        Compute A (OR of all conceptors)
+        :param conceptors:
+        :return:
+        """
+        # OR for all conceptor
+        for i, c in enumerate(conceptors):
+            if i == 0:
+                A = c
+            else:
+                A = c.logical_or(A)
+            # end if
+        # end for
+        return A
+    # end compute_A
+
+    # Compute quota
+    @staticmethod
+    def compute_quota(conceptors):
+        """
+        Compute quota
+        :param conceptors:
+        :return:
+        """
+        # Singular values of A
+        S = ConceptorPool.compute_A_SV(conceptors)
+
+        # Quota is the sum of SV
+        return float(
+            torch.mean(S)
+        )
+    # end compute_quota
 
 # end __ConceptorPool__
