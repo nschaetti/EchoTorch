b001f0a12afc5c22da68834217ea7c432fdb824f
nschaetti <n.schaetti@gmail.com>
2019-10-29 10:14:29 +0100

    Can reset states or not with reset_state

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
+
+# Imports
 from setuptools import setup, find_packages
 
-setup(name='EchoTorch',
-      version='0.1.2',
-      description="A Python toolkit for Reservoir Computing.",
-      long_description="A Python toolkit for Reservoir Computing and Echo State Network experimentation based on pyTorch.",
-      author='Nils Schaetti',
-      author_email='nils.schaetti@unine.ch',
-      license='GPLv3',
-      packages=find_packages(),
-      install_requires=[
-          'torch',
-          'numpy',
-          'torchvision'
-      ],
-      zip_safe=False
-      )
+# Installation setup
+setup(
+    name='EchoTorch',
+    version='0.1.2',
+    description="A Python toolkit for Reservoir Computing.",
+    long_description="A Python toolkit for Reservoir Computing, Echo State Network and Conceptor experimentation based on pyTorch.",
+    author='Nils Schaetti',
+    author_email='nils.schaetti@unine.ch',
+    license='GPLv3',
+    packages=find_packages(),
+    install_requires=[
+      'torch==1.3.0',
+      'torchvision==0.4.1'
+    ],
+    zip_safe=False
+)
 
