2246707bfa43ee175db3ce7f1597b67b87a9572c
schaetti <snow2458-->
2019-11-03 19:23:38 +0100

    Test __init__ file

---
+++ test/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# File : test/__init__
+# Description : Tests subpackage init file.
+# Date : Third of November, 2019
+#
+# This file is part of EchoTorch.  EchoTorch is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Nils Schaetti <nils.schaetti@unine.ch>
+
+
+# Imports
+from .EchoTorchTestCase import EchoTorchTestCase
+from .test_narma10_prediction import Test_NARMA10_Prediction
+from .test_subspace_first_demo import Test_Subspace_First_Demo
