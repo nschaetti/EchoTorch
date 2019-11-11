199019cbfcd56dfbcde449bcbf78f5cee915a219
schaetti <snow2458-->
2019-11-11 17:15:54 +0100

    Work in progress on schaetti2016

--- papers/schaetti2016/ESNs-Based-RC-for-MNIST-Handwritten-Digits-Recognition.py
+++ papers/schaetti2016/ESNs-Based-RC-for-MNIST-Handwritten-Digits-Recognition.py
@@ -20,9 +20,11 @@
 # Copyright Nils Schaetti <nils.schaetti@unine.ch>
 
 # Imports
-import torch
 import torch.utils.data
+from torch.autograd import Variable
 import torchvision.datasets
+import echotorch.datasets
+import echotorch.nn.reservoir
 import matplotlib.pyplot as plt
 
 
@@ -30,44 +32,76 @@ import matplotlib.pyplot as plt
 reservoir_size = 500
 spectral_radius = 0.99
 leaky_rate = 0.5
+batch_size = 8
 
 # MNIST data set train
 train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST(
-        root=".",
-        train=True,
-        download=True,
-        transform=torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.1307,), (0.3081,))
-        ])
+    echotorch.datasets.ImageToTimeseries(
+        torchvision.datasets.MNIST(
+            root=".",
+            train=True,
+            download=True,
+            transform=torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,))
+            ])
+        ),
+        n_images=10
     ),
-    batch_size=1,
+    batch_size=batch_size,
     shuffle=True
 )
 
 # MNIST data set test
 test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST(
-        root=".",
-        train=False,
-        download=True,
-        transform=torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.1307,), (0.3081,))
-        ])
+    echotorch.datasets.ImageToTimeseries(
+        torchvision.datasets.MNIST(
+            root=".",
+            train=False,
+            download=True,
+            transform=torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,))
+            ])
+        ),
+        n_images=10
     ),
-    batch_size=1,
+    batch_size=batch_size,
     shuffle=True
 )
 
+
+# Tranpose class index to one-hot vector
+def to_one_hot(x):
+    """
+    Transpose class index to one-hot vector
+    :param x:
+    :return:
+    """
+    x = x.long()
+    return torch.eye(10).index_select(0, x.data)
+# end to_one_hot
+
+
+# Matrices generator
+
+# Instanciate ESN module
+esn = echotorch.nn.reservoir.LiESN(
+    input_dim=28,
+    hidden_dim=reservoir_size,
+    output_dim=10,
+    leaky_rate=0.5,
+    spectral_radius=0.99
+)
+
 # For each training sample
-for batch_idx, (data, target) in enumerate(train_loader):
-    print(data.shape)
-    print(target)
-    plt.imshow(data[0][0], cmap='gray')
-    plt.show()
-    if batch_idx == 10:
-        break
-    # end if
+for batch_idx, (data, targets) in enumerate(train_loader):
+    # Remove channel
+    data = data.reshape(batch_size, 280, 28)
+
+    # To Variable
+    inputs, targets = Variable(data, requires_grad=False), Variable(targets, requires_grad=False)
+
+    # Feed ESN
+
 # end for
