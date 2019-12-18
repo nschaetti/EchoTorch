8ad1f670caaa5d84443bef1062e3e2e336ca662d
nschaetti <n.schaetti@gmail.com>
2019-12-18 16:14:05 +0100

    memory_management.py : Memory management demo without debugging

--- examples/conceptors/memory_management.py
+++ examples/conceptors/memory_management.py
@@ -27,11 +27,11 @@ import echotorch.utils.matrix_generation as mg
 import argparse
 import echotorch.utils
 import echotorch.datasets as etds
-import echotorch.utils.visualisation as ecvs
 from echotorch.datasets import DatasetComposer
 from torch.utils.data.dataloader import DataLoader
 import matplotlib.pyplot as plt
 from torch.autograd import Variable
+from echotorch.nn import Node
 
 # Random numb. init
 torch.random.manual_seed(1)
@@ -39,44 +39,47 @@ np.random.seed(1)
 
 # region PARAMS
 
+# 1. In this section, we set all the different parameters
+# used in this experiment.
+
 # Type params
 dtype=torch.float64
 
-# Reservoir params
+# Reservoir parameters
 reservoir_size = 100
 spectral_radius = 1.5
-bias_scaling = 0.2
+bias_scaling = 0.25
 connectivity = 10.0 / reservoir_size
 
-# Inputs params
+# Inputs parameters
 input_scaling = 1.5
 
-# Sequence lengths
+# Washout and learning lengths
 washout_length = 100
 learn_length = 100
 
-# Test params
+# Testing parameters
 interpolation_rate = 20
 conceptor_test_length = 200
 conceptor_test_washout = 200
 
-# Regularization params
+# Regularization parameters
 ridge_param_wout = 0.01
 
-# Plot params
+# Ploting parameters
 signal_plot_length = 20
 n_plot_singular_values = 100
 
-# Aperture
+# Aperture parameter
 aperture = 1000
 
-# Pattern params
+# Pattern parameters
 n_patterns = 16
 
 # endregion PARAMS
 
 # Argument parsing
-parser = argparse.ArgumentParser(prog="subspace_demo", description=u"Fig. 1 BC subspace first demo")
+parser = argparse.ArgumentParser(prog="memory_management", description="Memory management (with debug)")
 parser.add_argument("--w", type=str, default="", required=False)
 parser.add_argument("--w-name", type=str, default="", required=False)
 parser.add_argument("--win", type=str, default="", required=False)
@@ -88,6 +91,10 @@ parser.add_argument("--x0-name", type=str, default="", required=False)
 args = parser.parse_args()
 
 # region MATRICES_INIT
+
+# 2. We generate matrices Wstar, Win and Wbias,
+# either from a random number generator or from
+# matlab files.
 
 # Load W from matlab file and random init ?
 if args.w != "":
@@ -169,90 +176,126 @@ else:
 
 # region CREATE_PATTERNS
 
+# 3. We create the different patterns to be loaded
+# into the reservoir and learned by the Conceptors.
+# There is 13 patterns, 3 are repeated (6, 7, 8)
+# to show that it does not increase memory size.
+
 # Pattern 1 (sine p=10)
-pattern1_training = etds.SinusoidalTimeseries(sample_len=washout_length + learn_length, n_samples=1, a=1,
+pattern1_training = etds.SinusoidalTimeseries(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
+    a=1,
     period=10,
     dtype=dtype
 )
 
 # Pattern 2 (sine p=15)
-pattern2_training = etds.SinusoidalTimeseries(sample_len=washout_length + learn_length, n_samples=1, a=1,
+pattern2_training = etds.SinusoidalTimeseries(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
+    a=1,
     period=15,
     dtype=dtype
 )
 
 # Pattern 3 (periodic 4)
-pattern3_training = etds.PeriodicSignalDataset(sample_len=washout_length + learn_length, n_samples=1,
+pattern3_training = etds.PeriodicSignalDataset(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
     period=[-0.4564, 0.6712, -2.3953, -2.1594],
     dtype=dtype
 )
 
 # Pattern 4 (periodic 6)
-pattern4_training = etds.PeriodicSignalDataset(sample_len=washout_length + learn_length, n_samples=1,
+pattern4_training = etds.PeriodicSignalDataset(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
     period=[0.5329, 0.9621, 0.1845, 0.5099, 0.3438, 0.7697],
     dtype=dtype
 )
 
 # Pattern 5 (periodic 7)
-pattern5_training = etds.PeriodicSignalDataset(sample_len=washout_length + learn_length, n_samples=1,
+pattern5_training = etds.PeriodicSignalDataset(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
     period=[0.8029, 0.4246, 0.2041, 0.0671, 0.1986, 0.2724, 0.5988],
     dtype=dtype
 )
 
 # Pattern 6 (sine p=12)
-pattern6_training = etds.SinusoidalTimeseries(sample_len=washout_length + learn_length, n_samples=1, a=1,
+pattern6_training = etds.SinusoidalTimeseries(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
+    a=1,
     period=12,
     dtype=dtype
 )
 
 # Pattern 7 (sine p=5)
-pattern7_training = etds.SinusoidalTimeseries(sample_len=washout_length + learn_length, n_samples=1, a=1,
+pattern7_training = etds.SinusoidalTimeseries(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
+    a=1,
     period=5,
     dtype=dtype
 )
 
 # Pattern 8 (sine p=6)
-pattern8_training = etds.SinusoidalTimeseries(sample_len=washout_length + learn_length, n_samples=1, a=1,
+pattern8_training = etds.SinusoidalTimeseries(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
+    a=1,
     period=6,
     dtype=dtype
 )
 
 # Pattern 9 (periodic 8)
-pattern9_training = etds.PeriodicSignalDataset(sample_len=washout_length + learn_length, n_samples=1,
+pattern9_training = etds.PeriodicSignalDataset(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
     period=[0.8731, 0.1282, 0.9582, 0.6832, 0.7420, 0.9829, 0.4161, 0.5316],
     dtype=dtype
 )
 
 # Pattern 10 (periodic 7)
-pattern10_training = etds.PeriodicSignalDataset(sample_len=washout_length + learn_length, n_samples=1,
+pattern10_training = etds.PeriodicSignalDataset(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
     period=[0.6792, 0.5129, 0.2991, 0.1054, 0.2849, 0.7689, 0.6408],
     dtype=dtype
 )
 
 # Pattern 11 (periodic 3)
-pattern11_training = etds.PeriodicSignalDataset(sample_len=washout_length + learn_length, n_samples=1,
+pattern11_training = etds.PeriodicSignalDataset(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
     period=[1.4101, -0.0992, -0.0902],
     dtype=dtype
 )
 
 # Pattern 12 (sine p=6)
-pattern12_training = etds.SinusoidalTimeseries(sample_len=washout_length + learn_length, n_samples=1, a=1,
+pattern12_training = etds.SinusoidalTimeseries(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
+    a=1,
     period=11,
     dtype=dtype
 )
 
 # Pattern 13 (periodic 5)
-pattern13_training = etds.PeriodicSignalDataset(sample_len=washout_length + learn_length, n_samples=1,
-    period=[0.3419, 1.1282, 0.3107, -0.8949, -0.7266],
+pattern13_training = etds.PeriodicSignalDataset(
+    sample_len=washout_length + learn_length,
+    n_samples=1,
+    period=[0.9, -0.021439412841318672, 0.0379515995051003, -0.9, 0.06663989939293802],
     dtype=dtype
 )
 
-
 # Composer
 dataset_training = DatasetComposer([
-    pattern1_training, pattern2_training, pattern3_training, pattern4_training, pattern1_training, pattern2_training,
-    pattern3_training, pattern5_training, pattern6_training, pattern7_training, pattern8_training, pattern9_training, pattern10_training, pattern11_training,
-    pattern12_training, pattern13_training
+    pattern1_training, pattern2_training, pattern3_training, pattern4_training, pattern5_training, pattern1_training,
+    pattern2_training, pattern3_training, pattern6_training, pattern7_training, pattern8_training, pattern9_training,
+    pattern10_training, pattern11_training, pattern12_training, pattern13_training
 ])
 
 # Data loader
@@ -262,29 +305,31 @@ patterns_loader = DataLoader(dataset_training, batch_size=1, shuffle=False, num_
 
 # region CREATE_CONCEPTORS
 
+# 4. We create a conceptor set, 16 conceptors,
+# and an incremental conceptor net (IncConceptorNet)
+
 # Create a set of conceptors
 conceptors = ecnc.ConceptorSet(input_dim=reservoir_size)
 
 # Create sixteen conceptors
 for p in range(n_patterns):
-    conceptors.add(0, ecnc.Conceptor(input_dim=reservoir_size, aperture=aperture, dtype=dtype))
+    conceptors.add(p, ecnc.Conceptor(input_dim=reservoir_size, aperture=aperture, dtype=dtype))
 # end for
 
 # Create a conceptor network using
 # an incrementing self-predicting ESN which
-# will learn sixteen conceptors
+# will learn sixteen patterns
 conceptor_net = ecnc.IncConceptorNet(
     input_dim=1,
     hidden_dim=reservoir_size,
     output_dim=1,
-    learning_algo='inv',
+    conceptor=conceptors,
     w_generator=w_generator,
     win_generator=win_generator,
     wbias_generator=wbias_generator,
-    input_scaling=1.0,
-    ridge_param=ridge_param_wout,
+    ridge_param_wout=ridge_param_wout,
+    aperture=aperture,
     washout=washout_length,
-    conceptor=conceptors,
     dtype=dtype
 )
 
@@ -292,19 +337,19 @@ conceptor_net = ecnc.IncConceptorNet(
 
 # region INCREMENTAL_LOADING
 
-# Save pattern for plotting
+# 5. We incrementally load the patterns into the reservoir
+# and we save the results for plotting and testing.
+
+# Save pattern for plotting, last state, A and quota after each loading
 P_collector = torch.empty(n_patterns, signal_plot_length, dtype=dtype)
-
-# Save last state
 last_states = torch.empty(n_patterns, reservoir_size, dtype=dtype)
-
-# Save A after each loading
 A_collector = list()
-
-# Save quota after each loading
 quota_collector = torch.zeros(n_patterns)
 
-# Go through dataset
+# Conceptors activated in the loop
+conceptor_net.conceptor_active(True)
+
+# For each sample in the dataset
 for p, data in enumerate(patterns_loader):
     # Inputs and labels
     inputs, outputs, labels = data
@@ -314,71 +359,111 @@ for p, data in enumerate(patterns_loader):
         inputs, outputs = Variable(inputs.double()), Variable(outputs.double())
     # end if
 
-    # Set conceptor to use
+    # Set the conceptor activated in
+    # the loop.
     conceptors.set(p)
 
-    # Feed SP-ESN
+    # Feed SPESN with inputs,
+    # output learning to recreate the inputs
+    # so the inputs are the targets.
     X = conceptor_net(inputs, inputs)
 
-    # Save pattern and states
+    # Finalize Conceptor by learning
+    # the Conceptor matrix from the
+    # neurons activation received in
+    # the preceding line.
+    conceptors[p].finalize()
+
+    # We change the aperture of the Conceptor,
+    # the Conceptor matrix C is modified.
+    conceptors[p].aperture = aperture
+
+    # We save the patterns to be plotted afterwards,
+    # we save the last state to start the generation.
+    # we also save the quota of the space used by the
+    # patterns currently loaded in the reservoir.
     P_collector[p] = inputs[0, washout_length:washout_length+signal_plot_length, 0]
     last_states[p] = X[0, -1]
     quota_collector[p] = conceptors.quota()
 
-    # Save A
-    if p != 0:
-        A_collector.append(conceptors.A())
-    else:
-        A_collector.append(ecnc.Conceptor.identity(reservoir_size))
-    # end if
+    # Save A = C1 OR ... OR Cn
+    # of the currently loaded patterns
+    # (with the aperture set in the
+    # parameters).
+    A_collector.append(conceptors.A())
 # end for
 
 # endregion INCREMENTAL_LOADING
 
 # region TEST
 
-# In evaluation mode
+# 6. We test the system by generating signals,
+# we align these with original patterns and
+# we measure its performances with NRMSE.
+
+# We are going to to some pattern
+# generation, so we stop the learning
+# and switch to the evaluation mode.
 conceptor_net.train(False)
 
-# Generated patterns aligned
+# We save the pattern generated that we will
+# aligned to the true pattern by finding
+# the best phase shift.
 generated_samples_aligned = np.zeros((n_patterns, signal_plot_length))
 
-# Save NRMSE of aligned patterns
+# We also save the NRMSE of the aligned
+# patterns to see how this system can remember
+# the loaded patterns.
 NRMSEs_aligned = torch.zeros(n_patterns)
 
-# Set conceptors in evaluation mode and generate a sample
+# For each pattern we generate a sample by filtering the neurons
+# activation with the selected Conceptor, we then align the
+# generated sample to the real pattern by testing different
+# phase shift and we save the result.
 for p in range(n_patterns):
-    # Set it as current conceptor
+    # Set the current conceptor
+    # corresponding to the pth pattern.
     conceptors.set(p)
 
     # Set last state in training phase as initial
-    # state here
+    # state here.
     conceptor_net.cell.set_hidden(last_states[p])
 
-    # Generate sample
+    # Generate sample, we give a zero input of size
+    # batch size x time length x number of inputs.
+    # We don't reset the state as we set the initial state
+    # just before.
     generated_sample = conceptor_net(torch.zeros(1, conceptor_test_length, 1, dtype=dtype), reset_state=False)
 
-    # Find best phase shift
+    # We find the best phase shift by interpolating the original
+    # and the generated signal quadratically and trying different
+    # shifts. We take the best under the NRMSE evaluation measure.
     generated_sample_aligned, _, NRMSE_aligned = echotorch.utils.pattern_interpolation(
         P_collector[p],
         generated_sample[0],
         interpolation_rate
     )
 
-    # Save
+    # Save aligned sample and the corresponding NRMSE
     generated_samples_aligned[p] = generated_sample_aligned
     NRMSEs_aligned[p] = NRMSE_aligned
 # end for
 
-# Show NRMSE
+# Show the average NRMSE
+print(u"Average NRMSE : {}".format(torch.mean(NRMSEs_aligned)))
 print(u"Average NRMSE except last : {}".format(torch.mean(NRMSEs_aligned[:-1])))
 
 # endregion TEST
 
-# region PLOT
+# region PLOTTING
+
+# 7. We plot the original patterns and the generated signal,
+# and the singular values of A (OR of all conceptor after loading
+# the pth pattern) to show the share of the reservoir used.
+# We show the NRMSE (lower right) and the quota (lower left).
 
 # Figure (square size)
-plt.figure(figsize=(16, 16))
+plt.figure(figsize=(20, 16))
 
 # Plot index
 plot_index = 0
@@ -394,7 +479,6 @@ for p in range(n_patterns):
     _, Sx, _ = A.SVD
 
     # Plot singular values of C(all)
-    plt.fill(np.linspace(0, signal_plot_length, n_plot_singular_values), 2.0 * Sx - 1.0, color='red', alpha=0.75)
     plt.fill_between(np.linspace(0, signal_plot_length, n_plot_singular_values), 2.0 * Sx - 1.0, -1, color='red', alpha=0.75)
 
     # Plot generated pattern and original
@@ -465,4 +549,4 @@ for p in range(n_patterns):
 # Show figure
 plt.show()
 
-# endregion PLOT
+# endregion PLOTTING
