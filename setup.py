7984ecb7263e191328efba290fd23f80efe02b8f
nschaetti <n.schaetti@gmail.com>
2021-01-22 22:38:31 +0100

    Version 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 # Installation setup
 setup(
     name='EchoTorch',
-    version='0.2.2',
+    version='0.2.3',
     description="A Python toolkit for Reservoir Computing",
     long_description="A Python toolkit for Reservoir Computing, Echo State Network and Conceptor experimentation "
                      "based on pyTorch",
